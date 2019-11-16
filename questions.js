0a222be18afac9b98a3753a6585521a35e73aa04
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2019-11-16 16:48:00 -0600

    qseudocode for javascript written

---
+++ questions.js
@@ -0,0 +1,12 @@
+function initQuiz() {
+//  Initialize "time remaining" variable
+//  Clicking the "Start Quiz" button starts the quiz, hides the landing container, and displays the quiz container
+//  Upon starting the quiz, the time remaining variable is assigned a value equal to 15 seconds * the number of questions and starts decreasing by 1 each second
+//  During the quiz, the "quiz question" field has the current question, and the answer buttons have the possible answers for that question
+//  When the user clicks one of the answer buttons, if it is the correct answer, the message "Correct" is displayed, and if not, the message "Incorrect" is displayed and 15 seconds deducted from the timer.  Either way, a new question is then loaded
+//  When the final question is answered or the timer reaches zero, the quiz container is hidden and the score container is displayed, where the user enters their initials
+
+
+}
+
+initQuiz();
